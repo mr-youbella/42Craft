2c681b72b121d51cf9ce0c9d6933b5b2d14f1ba3
Younes Oubellal <younesoubllal@gmail.com>
2026-05-09 14:30:34 +0100

    initialize project with Next.js, TypeScript, and Tailwind CSS

---
+++ app/profile/page.tsx
@@ -0,0 +1,8 @@
+export default function Profile()
+{
+	return (
+		<div className="min-h-screen bg-linear-to-b from-[#0f1726] to-[#14202f]">
+			<div className="h-2 bg-amber-600`"></div>
+		</div>		
+	);
+}
